871c9ca3bb85504cbc5febc035208e8568192ccc
FrankPerez-23 <1650849@senati.pe>
2026-04-03 18:42:01 -0500

    conexion.php modificacion

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,20 +1,25 @@
 <?php
 // includes/conexion.php
-$host = 'electro-hogar-proyecto-practica-electrohogar13-8038.b.aivencloud.com';
-$port = '26873';
-$dbname = 'defaultdb';
-$username = 'avnadmin';
-$password = 'AVNS_Fwf7wKQJzyI6s9iq2cp';
+$host = 'sql110.infinityfree.com';
+$port = '3306'; // Puerto por defecto de InfinityFree
+$dbname = 'if0_41533960_electrohogar';
+$username = 'if0_41533960';
+$password = 'Tilin2326'; 
 
 try {
+    // Agregamos port=$port a la cadena DSN
     $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
-    $options = [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-    ];
-    $pdo = new PDO($dsn, $username, $password, $options);
+    $conexion = new PDO($dsn, $username, $password);
+    
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // Esto asegura que los datos salgan como arreglos asociativos
+    $conexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); 
+    
 } catch(PDOException $e) {
     header('Content-Type: application/json');
-   // die(json_encode(['status' => 'error', 'msg' => 'Error de conexión a la nube']));
+    die(json_encode([
+        'status' => 'error', 
+        'msg' => 'Error de conexión: ' . $e->getMessage()
+    ]));
 }
+?>
