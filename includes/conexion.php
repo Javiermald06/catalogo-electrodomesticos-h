4a29bbddea8ce242d041df18719fe3155e7d735f
FrankPerez-23 <1650849@senati.pe>
2026-04-03 19:06:34 -0500

    conexion.php ultimo

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,19 +1,19 @@
 <?php
 // includes/conexion.php
 $host = 'sql110.infinityfree.com';
-$port = '3306'; // Puerto por defecto de InfinityFree
+$port = '3306'; 
 $dbname = 'if0_41533960_electrohogar';
 $username = 'if0_41533960';
 $password = 'Tilin2326'; 
 
 try {
-    // Agregamos port=$port a la cadena DSN
     $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
-    $conexion = new PDO($dsn, $username, $password);
     
-    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // Esto asegura que los datos salgan como arreglos asociativos
-    $conexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); 
+    // Cambiamos $conexion por $pdo para que sea compatible con tus APIs
+    $pdo = new PDO($dsn, $username, $password);
+    
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); 
     
 } catch(PDOException $e) {
     header('Content-Type: application/json');
